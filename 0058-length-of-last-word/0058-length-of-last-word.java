df752f4f62d1a6f32b8af13e92b794d47ae53132
DachshundMango <tedbae0504@gmail.com>
2024-11-04 10:40:06 +1030

    Time: 0 ms (100%), Space: 41.6 MB (49.37%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -8,18 +8,10 @@ class Solution {
         for (int i = (s.length() - 1); 0 <= i; i--) {
 
 
-            if ((countStart == false) && (s.charAt(i) ==  ' ')) {
-                continue;
-            }
-
             if ((countStart == false) && (s.charAt(i) !=  ' ')) {
                 countStart = true;
                 result = i + 1;
                 continue;
-            }
-
-            if ((countStart == true) && (s.charAt(i) !=  ' ')) {
-                continue; 
             }
 
             if ((countStart == true) && (s.charAt(i) ==  ' ')) {
