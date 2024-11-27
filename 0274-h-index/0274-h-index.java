a9b4cbc7716677c4353d745c5bfac73551ad8a3d
DachshundMango <tedbae0504@gmail.com>
2024-11-27 17:26:38 +1030

    Time: 5 ms (9.99%), Space: 41.5 MB (55.86%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,9 +1,8 @@
 class Solution {
     public int hIndex(int[] citations) {
         
-        
-
         int[] order = new int[citations.length];
+        int result = 0;
 
         for (int i = 0; i < order.length; i++) {
             int citNumber = 0;
@@ -16,20 +15,16 @@ class Solution {
             }
             order[i] = citNumber;
             citations[citIndex] = -1;
-        }
 
-        int result = 0;
-
-        for (int i = 0; i < order.length; i++) {
             if ((i + 1) > order[i]) {
                 result = i;
                 break;
             }
             result = i + 1;
+
         }
 
         return result;
-
 
     }
 }
