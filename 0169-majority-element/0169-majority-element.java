656c5d284f0947e21f239fa92ac9be22659fe269
DachshundMango <tedbae0504@gmail.com>
2024-11-29 17:33:47 +1030

    Time: 14 ms (25.35%), Space: 49.4 MB (82.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,29 +2,33 @@ class Solution {
     public int majorityElement(int[] nums) {
 
         int result = 0;
-
+        String checkedNumber = ",";
 
         for (int i = 0; i < nums.length; i++) {
-
+            
             int key = nums[i];
             int count = 0;
 
-            for (int j = 0; j < nums.length; j++) {
-                if (key == nums[j]) {
-                    count++;
+            if (!checkedNumber.contains("," + key + ",")) {
+                
+                for (int j = i; j < nums.length; j++) {
+                    if (key == nums[j]) {
+                        count++;
+                    } 
                 }
-            }
+                
+                if (count > (nums.length / 2)) {
+                    result = key;
+                    break;
+                } 
 
-            if (count > (nums.length / 2)) {
-                result = key;
-                break;
-            }
+                checkedNumber += "" + key + ",";
 
+            }
+       
         }
 
         return result;
-
     }
-
 }
         
