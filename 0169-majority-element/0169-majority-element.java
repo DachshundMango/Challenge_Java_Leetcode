f30463bfe3814a34014bb180cbf6a643e245a90a
DachshundMango <tedbae0504@gmail.com>
2024-12-22 15:28:59 +1030

    Time: 17 ms (15.28%), Space: 49 MB (93.68%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -7,20 +7,16 @@ class Solution {
 
         HashMap<Integer, Integer> resultMap = new HashMap<>();
 
+        int result = 0;
+
         for (int num : nums) {
             if (!resultMap.containsKey(num)) {
                 resultMap.put(num, 1);
             } else {
                 resultMap.put(num, resultMap.get(num) + 1);
             }
-
-        }
-
-        int result = 0;
-
-        for (Map.Entry<Integer, Integer> entry : resultMap.entrySet()) {
-            if (entry.getValue() > nums.length / 2) {
-                result = entry.getKey();
+            if (resultMap.get(num) > nums.length / 2) {
+                result = num;
                 break;
             }
         }
