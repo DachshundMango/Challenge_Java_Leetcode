23cd26d276ea23a558685f859ab055bfe9c335a4
DachshundMango <tedbae0504@gmail.com>
2024-12-25 17:58:07 +1030

    Time: 2 ms (12.78%), Space: 40.9 MB (97.54%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -3,28 +3,37 @@ import java.util.Map;
 
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        
+
+        boolean result = true;
+
         String[] words = s.split(" ");
-        if (pattern.length() != words.length) return false;
 
-        Map<Character, String> charToWord = new HashMap<>();
-        Map<String, Character> wordToChar = new HashMap<>();
-
-        for (int i = 0; i < pattern.length(); i++) {
-            char c = pattern.charAt(i);
-            String word = words[i];
-
-            if (charToWord.containsKey(c) && !charToWord.get(c).equals(word)) {
-                return false;
-            }
-            if (wordToChar.containsKey(word) && wordToChar.get(word) != c) {
-                return false;
-            }
-
-            charToWord.put(c, word);
-            wordToChar.put(word, c);
+        if (pattern.length() != words.length) {
+            return false;
         }
 
-        return true;
+
+        Map<Character, String> patternMap = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            if (patternMap.containsKey(pattern.charAt(i))) {
+                if (!patternMap.get(pattern.charAt(i)).equals(words[i])) {
+                    result = false;
+                    break;
+                }
+            }
+            if (patternMap.containsValue(words[i])) {
+                for (Map.Entry<Character, String> entry : patternMap.entrySet()) {
+                    if (entry.getValue().equals(words[i]) && !entry.getKey().equals(pattern.charAt(i))) {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            patternMap.put(pattern.charAt(i), words[i]);
+        }
+
+        return result;                                                       
+        
     }
 }
