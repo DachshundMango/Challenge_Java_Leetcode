e8161446873aa6d63d5e8525245a5d3cd05ebce3
DachshundMango <tedbae0504@gmail.com>
2025-01-02 10:49:42 +1030

    Time: 31 ms (6.14%), Space: 43.1 MB (81.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,12 +4,10 @@ import java.util.Map;
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        if (t.length() != s.length()) {
+       if (t.length() != s.length()) {
             return false;
         }
-
         int commonLength = s.length();
-        boolean result = true;
         Map<Character, Integer> checkMap = new HashMap<>();
 
         for (int i = 0; i < commonLength; i++) {
@@ -18,20 +16,17 @@ class Solution {
             } else {
                 checkMap.put(s.charAt(i), 1);
             }
-        }
-        for (int i = 0; i < commonLength; i++) {
-            if (!checkMap.containsKey(t.charAt(i))) {
-                result = false;
-                break;
-            } else {
+            if (checkMap.containsKey(t.charAt(i))) {
                 checkMap.put(t.charAt(i), checkMap.get(t.charAt(i)) - 1);
-                if (checkMap.get(t.charAt(i)) < 0) {
-                    result = false;
-                    break;
-                }
+            } else {
+                checkMap.put(t.charAt(i), -1);
             }
+            
+            
         }
-
+        
+        boolean result = true;
+        
         for (Integer value : checkMap.values()) {
             if (value != 0) {
                 result = false;
